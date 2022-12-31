5c352cc34091278cede3d4dbc2b870e8e2213758
Ashish Sharda <ashishjsharda@gmail.com>
2022-12-31 07:50:04 -0600

    update function

---
+++ target/debug/deps/adder-0e05c54970b82808.d
@@ -0,0 +1,5 @@
+C:\Users\ashis\CLionProjects\rusttut\src\adder\target\debug\deps\adder-0e05c54970b82808.exe: src\lib.rs
+
+C:\Users\ashis\CLionProjects\rusttut\src\adder\target\debug\deps\adder-0e05c54970b82808.d: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/adder-1bb7a7c51071cdcb.d
@@ -0,0 +1,7 @@
+C:\Users\ashis\CLionProjects\rusttut\src\adder\target\debug\deps\adder-1bb7a7c51071cdcb.rmeta: src\lib.rs
+
+C:\Users\ashis\CLionProjects\rusttut\src\adder\target\debug\deps\libadder-1bb7a7c51071cdcb.rlib: src\lib.rs
+
+C:\Users\ashis\CLionProjects\rusttut\src\adder\target\debug\deps\adder-1bb7a7c51071cdcb.d: src\lib.rs
+
+src\lib.rs:
